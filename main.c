0fbad43982cb9c98bf18819cd5c9172fed08541b
yara <yaralm5@hotmail.com>
2024-10-14 17:43:17 -0300

    Definição de size para os vetores

--- main.c
+++ main.c
@@ -1,11 +1,15 @@
 #include <stdio.h> 
 #include "funcoes.h"
+#define size_1 1000
+#define size_2 100000
+#define size_3 1000000
+
 
 int main(int argc, char const *argv[])
 {
-    int v1[1000]; 
-    int v2[100000]; 
-    int v3[1000000];
+    int v1[size_1]; 
+    int v2[size_2]; 
+    int v3[size_3];
 
     
     return 0;
